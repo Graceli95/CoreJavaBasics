f333eb57f52bb38395d3c378acf87eff83ef2b06
Graceli95 <graceli9095@gmail.com>
2024-11-17 21:29:16 -0700

    Initial commit

---
+++ src/com/perscholas/java_basics/AddTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.perscholas.java_basics;
+
+public class Practice1 {
+  int x = 5;
+  int y = 10;
+  int a = x + y;
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
+
+}
---
+++ src/com/perscholas/java_basics/DoubleDivisionAndCast.java
@@ -0,0 +1,14 @@
+package com.perscholas.java_basics;
+
+public class DoubleDivision {
+    public static void main(String[] args) {
+       double num1 = 3.14;
+       double num2 = 1.5;
+       double result = num1 / num2;
+       System.out.println(result);
+//       cast the double to an integer, (int) is the explicit cast operator.
+//       Casting from double to int does not round the value; it simply removes the fractional part.
+       int intVariable = (int) result;
+       System.out.println(intVariable);
+    }
+}
